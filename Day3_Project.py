e480e35ca9fb4b396f4bbfc1af28df2721145095
Man_Of_Darkness <72191625+WiTe0DeViL@users.noreply.github.com>
2021-08-22 20:41:59 +0530

    Update Day3_Project.py

--- Day3_Project.py
+++ Day3_Project.py
@@ -1,1 +1,8 @@
+import random as r # importing modules
+import string as s
 
+# OTP generator
+OTP_PIN = list(s.ascii_letters + s.digits)
+for i in range(6):
+  otp = r.choice(OTP_PIN)
+  print(otp, end="")
